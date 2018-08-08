ca116f049f1c32aa25b8c4728bb62e7d89207461
Nicholas Williams <newilliams2@ml-c02w703chtd6.wsgc.com>
2018-08-08 14:37:01 -0700

    add autofill Google maps input directive

--- app.js
+++ app.js
@@ -20,9 +20,30 @@ beachOrNah.config(function ($routeProvider) {
 
 // CONTROLLERS
 beachOrNah.controller('homeController', ['$scope', function($scope) {
-
+	$scope.gPlace;
 }]);
 
 beachOrNah.controller('forecastController', ['$scope', function($scope) {
 
 }]);
+
+// DIRECTIVES
+beachOrNah.directive('googleplace', function () {
+	return {
+		//binding this 
+		require: 'ngModel',
+		link: function(scope, element, attrs, model) {
+			let options = {
+				types: []
+			};
+
+			scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+			google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+				scope.$apply(function () {
+					model.$setViewValue(element.val());
+				});
+			});
+		}
+	};
+});
