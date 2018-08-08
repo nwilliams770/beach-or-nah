52b30b42c411ed45c66f0e197afbbd6c9e5ebe50
Nicholas Williams <newilliams2@ml-c02w703chtd6.wsgc.com>
2018-08-08 15:54:03 -0700

    add placeService

--- app.js
+++ app.js
@@ -17,13 +17,26 @@ beachOrNah.config(function ($routeProvider) {
 	})
 })
 
+// SERVICES
+beachOrNah.service('placeService', function() {
+	this.place = "";
+})
 
 // CONTROLLERS
-beachOrNah.controller('homeController', ['$scope', function($scope) {
+beachOrNah.controller('homeController', ['$scope', 'placeService', function($scope, placeService) {
 	$scope.gPlace;
+
+	placeService.place = $scope.chosenPlace;
+ 
+	$scope.$watch('chosenPlace', function() {
+		placeService.place = $scope.chosenPlace;
+	})
+
+	console.log($scope);
 }]);
 
-beachOrNah.controller('forecastController', ['$scope', function($scope) {
+beachOrNah.controller('forecastController', ['$scope', 'placeService', function($scope, placeService) {
+	$scope.chosenPlace = placeService.place;
 
 }]);
 
@@ -40,6 +53,7 @@ beachOrNah.directive('googleplace', function () {
 			scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
 
 			google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+				// Explore this part more, why are we using $apply
 				scope.$apply(function () {
 					model.$setViewValue(element.val());
 				});
