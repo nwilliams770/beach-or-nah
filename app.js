8b52c701f1714093cfdafbfd041745dbbd739ebc
Nicholas Williams <nwilliams770@gmail.com>
2018-12-10 17:51:31 -0800

    add cookie

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const beachOrNah = angular.module('beachOrNah', ['ngRoute', 'ngResource', 'ngSanitize']);
+const beachOrNah = angular.module('beachOrNah', ['ngRoute', 'ngResource', 'ngSanitize', 'ngCookies']);
 
 beachOrNah.filter('removeCommas', function() {
     return function(value) {
-		let rounded = parseInt(value, 10); //convert to int
-		return rounded.toString().replace(",", "");
+        let rounded = parseInt(value, 10); //convert to int
+        return rounded.toString().replace(",", "");
     };
 });
 
 // SANITIZE RESOURCE 
 beachOrNah.config(['$sceDelegateProvider', function($sceDelegateProvider) {
-	$sceDelegateProvider.resourceUrlWhitelist([
-	'self',
-	'http://api.openweathermap.org/data/2.5/forecast?APPID=dfb10e52305b309e27a290c220279d28']);
+    $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'http://api.openweathermap.org/data/2.5/forecast?APPID=dfb10e52305b309e27a290c220279d28']);
 }]);
 
 // SERVICES
 beachOrNah.service('placeService', function() {
-	this.place = "";
+    this.place = "";
 });
